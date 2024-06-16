4a828d7071620fe582deaea77ada2540d8bb0a79
Ahmet Çevrim <purplecevrim@gmail.com>
2024-06-16 16:15:48 +0300

    Daily Football Grid Updated
    16.06 Update

--- FutbolGrid/script.js
+++ FutbolGrid/script.js
@@ -1,8 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
     const grid = document.querySelector('.grid');
-    for(let i = 0; i < 16; i++) {
+    for (let i = 0; i < 16; i++) {
         const cell = document.createElement('div');
         grid.appendChild(cell);
+    }
+
+    // Özel hücrelerin numaraları
+    const specialCells = [0,1,2,3,4,8,12];
+
+    for (let i = 0; i < 16; i++) {
+        if (specialCells.includes(i)) {
+            grid.children[i].classList.add('special-cell');
+        }
     }
     document.querySelector('.grid').children[0].innerHTML = "<img src='images/const/logo.png' alt='Logo'>";
     document.querySelector('.grid').children[1].innerHTML = "<img src='images/hmm/netherlands.png' alt='Nation1'>";
@@ -11,22 +20,50 @@ document.addEventListener("DOMContentLoaded", function() {
     document.querySelector('.grid').children[4].innerHTML = "<img src='images/hmm/fenerbahce.png' alt='Team1'>";
     document.querySelector('.grid').children[8].innerHTML = "<img src='images/hmm/galatasaray.png' alt='Team2'>";
     document.querySelector('.grid').children[12].innerHTML = "<img src='images/hmm/besiktas.png' alt='Team3'>";
-    // Enter tuşuna basıldığında submit işlemi gerçekleştirilsin
+
     document.getElementById("predictionInput").addEventListener("keydown", function(event) {
         if (event.key === "Enter") {
             submitPrediction();
         }
     });
+
+    // Modal penceresi açılışı
+    var modal = document.getElementById("tutorialModal");
+    var closeButton = document.querySelector(".close-button");
+    var startButton = document.getElementById("startButton");
+
+    // Modal aç
+    modal.style.display = "block";
+
+    // Modal kapat butonuna tıklayınca kapat
+    closeButton.addEventListener("click", function() {
+        modal.style.display = "none";
+    });
+
+    // Başla butonuna tıklayınca kapat
+    startButton.addEventListener("click", function() {
+        modal.style.display = "none";
+    });
+
+    // Modal dışında bir yere tıklanınca kapat
+    window.addEventListener("click", function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    });
 });
+
 var cellLocked = new Array(16).fill(false);
 
 function submitPrediction() {
+    var kontrol = false;
     for (let index = 0; index < cellLocked.length; index++) {
-        check(index);
-        
+        if(kontrol==false)
+            check(index);
     }
     document.getElementById("predictionInput").value = "";
 }
+
 function check(index){
     if(cellLocked[index]){
         return;
@@ -34,8 +71,7 @@ function check(index){
 
     var predictionInput = document.getElementById("predictionInput").value;
     console.log("Tahmin: " + predictionInput);
-            
-    // XML dosyasından veri çekme
+
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
         if (xhr.readyState == 4 && xhr.status == 200) {
@@ -46,18 +82,16 @@ function check(index){
                 var imageURL = predictions[i].getElementsByTagName("imageURL")[0].childNodes[0].nodeValue;
                 for (var j = 0; j < keywords.length; j++) {
                     var keyword = keywords[j].childNodes[0].nodeValue;
-                    if (predictionInput.toLowerCase() === keyword) {
-                        // Tahmin doğruysa, XML'den alınan görsel URL'ini kullanarak görseli grid'e ekle
+                    if (predictionInput.toLowerCase() === keyword.toLowerCase()) {
                         document.querySelector('.grid').children[index].innerHTML = "<img src='" + imageURL + "' alt='" + predictionInput + " Görseli'>";
+                        kontrol = true;
                         cellLocked[index] = true;
-                        return; // Tahmin doğruysa, döngüden çık
+                        return;
                     }
                 }
             }
-            // Tahmin doğru değilse uyarı ver
-            //alert("Tahmin yanlış veya geçersiz!");
         }
     };
-    xhr.open("GET", "predictions.xml", true); // XML dosyasının adını buraya yazın
+    xhr.open("GET", "predictions.xml", true);
     xhr.send();
 }
